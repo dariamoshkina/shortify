aecab58cbae5192b492fc14c05328a5cb887e04d
Daria Moshkina <dariamoshkina1@gmail.com>
2025-10-14 01:53:50 +0400

    Add cli params

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -6,22 +6,24 @@ import (
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 
+	"github.com/dariamoshkina/shortify/internal/config"
 	"github.com/dariamoshkina/shortify/internal/handler"
 	"github.com/dariamoshkina/shortify/internal/repository/inmemory"
 	"github.com/dariamoshkina/shortify/internal/service"
 )
 
 func main() {
-	baseURL := "http://localhost:8080"
+	appConfig := config.Parse()
+
 	repo := inmemory.NewInMemoryURLRepository()
-	shortener := service.NewShortenerService(repo, baseURL)
+	shortener := service.NewShortenerService(repo, appConfig.BaseURL)
 	urlHandler := handler.NewURLHandler(shortener)
 
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
 	r.Mount("/", urlHandler.Routes())
 
-	if err := http.ListenAndServe(":8080", r); err != nil {
+	if err := http.ListenAndServe(appConfig.Addr, r); err != nil {
 		panic(err)
 	}
 }
