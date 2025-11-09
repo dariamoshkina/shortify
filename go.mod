4643950b4ea9efd3fd31de7f672cb69e2245dd7e
Daria Moshkina <dariamoshkina1@gmail.com>
2025-11-09 14:40:21 +0300

    Add env parsing

--- go.mod
+++ go.mod
@@ -3,7 +3,9 @@ module github.com/dariamoshkina/shortify
 go 1.22.2
 
 require (
+	github.com/caarlos0/env/v6 v6.10.1
 	github.com/go-chi/chi/v5 v5.2.3
+	github.com/samber/lo v1.52.0
 	github.com/stretchr/testify v1.11.1
 )
 
@@ -11,5 +13,6 @@ require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/stretchr/objx v0.5.2 // indirect
+	golang.org/x/text v0.22.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- internal/config/config.go
+++ internal/config/config.go
@@ -1,6 +1,17 @@
 package config
 
-import "flag"
+import (
+	"flag"
+	"fmt"
+
+	"github.com/caarlos0/env/v6"
+	"github.com/samber/lo"
+)
+
+const (
+	DefaultServerAddress = "localhost:8080"
+	DefaultBaseURL       = "http://localhost:8080"
+)
 
 type Config struct {
 	Addr    string
@@ -8,13 +19,21 @@ type Config struct {
 }
 
 func Parse() *Config {
-	addr := flag.String("a", "localhost:8080", "host URL")
-	baseURL := flag.String("b", "http://localhost:8080", "base URL")
+	var (
+		config        Config
+		addr, baseURL *string
+	)
 
+	if err := env.Parse(&config); err != nil {
+		fmt.Println(err)
+	}
+
+	addr = flag.String("a", DefaultServerAddress, "host URL")
+	baseURL = flag.String("b", DefaultBaseURL, "base URL")
 	flag.Parse()
 
-	return &Config{
-		Addr:    *addr,
-		BaseURL: *baseURL,
-	}
+	config.Addr, _ = lo.Coalesce(config.Addr, *addr)
+	config.BaseURL, _ = lo.Coalesce(config.BaseURL, *baseURL)
+
+	return &config
 }
